43b726daba6e463d94e0d643f9a8a0d949290470
Biraj Adhikari <adhikari.biraj1@gmail.com>
2021-02-28 09:26:10 -0600

    added User as FK

--- info/models.py
+++ info/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Project(models.Model):
@@ -6,14 +7,16 @@ class Project(models.Model):
     description = models.CharField(max_length=200)
     repository = models.CharField(max_length=100)
     image = models.ImageField(upload_to='p_images')
+    done_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="list_project")
 
     def __str__(self):
-        return "Project Name: "+str(name)+" Link: "+str(repository)
+        return "Project Name: "+str(self.name)+" Link: "+str(self.repository)
 
 class Award(models.Model):
     name = models.CharField(max_length=70)
     sponsor = models.CharField(max_length=100)
     description = models.CharField(max_length=400)
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="list_award")
 
     def __str__(self):
-        return str(name)+" awarded by "+str(sponsor)
+        return str(self.name)+" awarded by "+str(self.sponsor)
