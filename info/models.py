e326f95609855a5c2975eb234b68a8f5e98a0b6a
Biraj Adhikari <adhikari.biraj1@gmail.com>
2021-03-03 10:10:02 -0600

    date field added to project

--- info/models.py
+++ info/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 # Create your models here.
 class Project(models.Model):
     name = models.CharField(max_length=70)
     description = models.CharField(max_length=200)
     repository = models.CharField(max_length=100)
+    start_date = models.DateField(default=datetime.date(2021,3,3))
+    end_date = models.DateField(default=datetime.date(2021,3,3))
     image = models.ImageField(upload_to='images/')
     done_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="list_project")
 
